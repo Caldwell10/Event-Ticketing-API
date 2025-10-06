ece7d906e18b40f79d54de1734c1e6dbe664285e
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-10-06 20:24:01 +0300

    feature: postgres DB setup

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# Load environement variable from .env file to prevent exposing sensitive DB credentials
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create a Session instance
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base class for class definitions
+Base = declarative_base()
+
+# Initialize dependency for database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
