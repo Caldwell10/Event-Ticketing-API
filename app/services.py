f7839a67245a684981624b855f261e58f55e97ec
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-10-06 21:50:48 +0300

    feature: added password hashing using bcrypt

---
+++ app/services.py
@@ -0,0 +1,17 @@
+import bcrypt
+from sqlalchemy.orm import Session
+
+# hash payload password before being stored in the database
+def hash_password(password: str):
+    """Hash a plaintext password."""
+    
+    # create a salt
+    salt = bcrypt.gensalt()
+
+    # hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password.decode('utf-8')
+
+
+    
